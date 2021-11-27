5ff9ffdeab67b377a59da7ef9e74e192d2aff1a7
Thomas Riffard <ferdodo@hotmail.fr>
2021-11-27 18:16:16 +0100

    review component layout

--- app/scripts/copy-to-dist.sh
+++ app/scripts/copy-to-dist.sh
@@ -2,5 +2,15 @@
 set -e
 mkdir -p dist
 cp index.html style.css dist
-cp -r node_modules/98.css/dist dist/style98
-cp -r node_modules/spectre.css/dist dist/spectre
+
+mkdir -p dist/style98
+cp node_modules/98.css/dist/98.css dist/style98
+cp node_modules/98.css/dist/ms_sans_serif.woff2 dist/style98
+cp node_modules/98.css/dist/ms_sans_serif_bold.woff2 dist/style98
+
+mkdir -p dist/spectre
+cp node_modules/spectre.css/dist/spectre.min.css dist/spectre
+cp node_modules/spectre.css/dist/spectre-icons.min.css dist/spectre
+
+mkdir -p dist/tailwind
+cp node_modules/tailwindcss/dist/tailwind.min.css dist/tailwind
