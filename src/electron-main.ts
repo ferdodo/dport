988c7dcf4b277119443fce58646a462009460302
Thomas Riffard <ferdodo@hotmail.fr>
2021-12-11 09:04:37 +0100

    use electron 16

--- src/electron-main.ts
+++ src/electron-main.ts
@@ -1,36 +1,46 @@
-import { app, BrowserWindow } from "electron";
-import initIpc from 'dport/lib/command/electron-main';
+import { app, BrowserWindow, ipcMain } from "electron";
+import { commandIpc, commandKillIpc } from 'dport/lib/command/electron-ipc';
+import { createMinimizeIpcDef, createCloseIpcDef } from 'dport/lib/window-handle/electron-ipc';
 
-(async function main() {
-	app.allowRendererProcessReuse = false;
-	await appReady();
-	await createWindow();
-	initIpc();
-})();
+export type electronIpcDefinition = {
+	name: string;
+	handler: (...any) => void | Promise<void>;
+};
 
-function appReady() {
-	return new Promise(function (resolve, reject) {
-		const timeout = setTimeout(reject, 5000, "Timeout !");
+initElectron();
 
-		app.on("ready", function () {
-			clearTimeout(timeout);
-			resolve(null);
-		});
-	});
+async function initElectron (){
+	try {
+		await app.whenReady();
+		const win = await createWindow();
+		const minimizeIpc = createMinimizeIpcDef(win);
+		const closeIpc = createCloseIpcDef(win);
+		registerIpc(minimizeIpc);
+		registerIpc(closeIpc);
+		registerIpc(commandIpc);
+		registerIpc(commandKillIpc);
+	} catch (error) {
+		console.error(error);
+		process.exit(-1);
+	}
 }
 
-async function createWindow() {
+async function createWindow() : Promise<BrowserWindow> {
 	const win = new BrowserWindow({
 		width: 1000,
 		height: 300,
 		frame: false,
 		webPreferences: {
 			nodeIntegration: true,
-			contextIsolation: false,
-            enableRemoteModule: true
+			contextIsolation: false
 		}
 	});
 
 	win.setMenuBarVisibility(false);
 	await win.loadFile("index.html");
+	return win;
+}
+
+function registerIpc(ipc: electronIpcDefinition){
+	ipcMain.handle(ipc.name, ipc.handler);
 }
--- src/lib/window-handle/electron.ts
+++ src/lib/window-handle/electron.ts
@@ -1,19 +1,13 @@
-import { remote } from "electron";
+import { ipcRenderer } from "electron";
 import { WindowHandleClass, WindowHandleInstance } from ".";
 
 const WindowHandle: WindowHandleClass = class implements WindowHandleInstance {
-	#window;
-
-	constructor(){
-		this.#window = remote.getCurrentWindow();
-	}
-
 	minimize() {
-		this.#window.minimize();
+		return ipcRenderer.invoke('minimize');
 	}
 
 	close() {
-		this.#window.close();
+		return ipcRenderer.invoke('close');
 	}
 
 	static makeDraggable(htmlElement){
