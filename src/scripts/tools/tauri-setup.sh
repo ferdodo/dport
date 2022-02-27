916736d12e673592b33282dd9375d618bc9f1cf4
Thomas Riffard <ferdodo@hotmail.fr>
2022-02-27 11:08:46 +0100

    fix: tauri using wrong config file

--- src/scripts/tools/tauri-setup.sh
+++ src/scripts/tools/tauri-setup.sh
@@ -15,6 +15,7 @@ esac
 # merge with personnal settings
 jq -s '.[0] * .[1]' src-tauri/tauri.conf.json tauri.conf.json > tmp.json
 mv tmp.json src-tauri/tauri.conf.json
+rm tauri.conf.json
 
 # grab version from package.json
 jq -s '[.[0], {package: {version: "\(.[1].version)" }}] | .[0] * .[1]' src-tauri/tauri.conf.json package.json > tmp.json
